519ddd542dfb7ab870d22ea0ccf684abd599fc7e
dbunalov <dbunalov@gmail.com>
2016-10-16 10:23:40 +0300

    NewUpd

--- ProgrammingFundamentals/FilesDirectoriesAndExceptions/05.FolderSize/FolderSize.cs
+++ ProgrammingFundamentals/FilesDirectoriesAndExceptions/05.FolderSize/FolderSize.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 
 
 namespace FolderSize
@@ -9,7 +10,17 @@ namespace FolderSize
     {
         static void Main()
         {
+            string[] files = Directory.GetFiles("TestFolder");
+            double sum = 0;
 
+            foreach (string file in files)
+            {
+                FileInfo fInfo = new FileInfo(file);
+                sum += fInfo.Length;
+            }
+
+            sum = sum / 1024 / 1024;
+            File.WriteAllText("output.txt", sum.ToString());
         }
     }
 }
