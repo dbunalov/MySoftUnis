27330b13a9c9c25b7d6ba35c0b2d2788f3eae890
dbunalov <dbunalov@gmail.com>
2016-10-16 10:04:44 +0300

    Upd 16-11-2016

---
+++ ProgrammingFundamentals/Objects and Classes/05.ClosestTwoPoints/ClosestTwoPoints.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace ClosestTwoPoints
+{
+    class ClosestTwoPoints
+    {
+        static void Main()
+        {
+            Point[] points = Readpoints();
+            Point[] closestPoints = FindClosestTwoPoints(points);
+            PrintDistance(closestPoints);
+            PrintPoint(closestPoints[0]);
+            PrintPoint(closestPoints[1]);
+            //double distance = CalcDistance(closestPoints[0], closestPoints[1]);
+
+            //foreach (Point point in points)
+            //{
+            //    PrintPoint(point);
+            //}
+
+        }
+        static Point[] FindClosestTwoPoints(Point[] points)
+        {
+            double minDistance = double.MaxValue;
+            Point[] closestTwoPoints = new Point[2];
+            for (int p1 = 0; p1 < points.Length; p1++)
+            {
+                for (int p2 = p1 + 1; p2 < points.Length; p2++)
+                {
+                    double distance = CalcDistance(points[p1], points[p2]);
+                    if (distance < minDistance)
+                    {
+                        minDistance = distance;
+                        closestTwoPoints[0] = points[p1];
+                        closestTwoPoints[1] = points[p2];
+                    }
+                }
+            }
+            return closestTwoPoints;
+        }
+        static void PrintDistance(Point[] points)
+        {
+            double distance = CalcDistance(points[0], points[1]);
+            Console.WriteLine($"{distance:f3}");
+        }
+
+        static void PrintPoint(Point point)
+        {
+            Console.WriteLine($"({point.X}, {point.Y})");
+        }
+
+        static double CalcDistance(Point p1, Point p2)
+        {
+            int deltaX = p2.X - p1.X;
+            int deltaY = p2.Y - p1.Y;
+            return Math.Sqrt(Math.Pow(deltaX, 2) + Math.Pow(deltaY, 2));
+        }
+        
+            static Point Readpoint()
+        {
+            int[] pointInfo = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Point point = new Point();
+            point.X = pointInfo[0];
+            point.Y = pointInfo[1];
+            return point;
+        }
+        static Point[] Readpoints()
+        {
+            int n = int.Parse(Console.ReadLine());
+            Point[] points = new Point[n];
+            for (int i = 0; i < n; i++)
+            {
+                points[i] = Readpoint();
+            }
+            return points;
+        }
+    }
+    class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+    }
+
+}
---
+++ ProgrammingFundamentals/Objects and Classes/07.SalesReport/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+
+
+namespace SalesReport
+{
+    class SalesReport
+    {
+        static void Main()
+        {
+            Sale[] sales = ReadSales();
+            string[] towns = sales
+                .Select(s => s.Town)
+                .Distinct() //takes non repeatable 
+                .OrderBy(t => t)
+                .ToArray();
+
+            foreach (string town in towns)
+            {
+                decimal salesByTown = sales
+                                        .Where(s => s.Town == town)//searching towns
+                                        .Select(s => s.Price * s.Quantity)
+                                        .Sum();
+                Console.WriteLine($"{town} -> {salesByTown:f2}");
+            }
+
+        }
+        static Sale ReadSale()
+        {
+            string[] items = Console.ReadLine().Split(' ');
+            return new Sale()
+            {
+                Town = items[0],
+                Product = items[1],
+                Price = decimal.Parse(items[2]),
+                Quantity = decimal.Parse(items[3])
+            };
+        }
+
+        static Sale[] ReadSales()
+        {
+            int n = int.Parse(Console.ReadLine());
+            Sale[] sales = new Sale[n];
+            for (int i = 0; i < n; i++)
+            {
+                sales[i] = ReadSale();
+            }
+            return sales;
+        }
+    }
+    class Sale
+    {
+        public string Town { get; set; }
+        public string Product { get; set; }
+        public decimal Price { get; set; }
+        public decimal Quantity { get; set; }
+    }
+    
+}
