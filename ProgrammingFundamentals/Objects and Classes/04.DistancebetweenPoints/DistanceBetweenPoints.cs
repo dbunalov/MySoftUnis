73879f4c1ecfd7352ce0f3e16f6393e2724cae0e
dbunalov <dbunalov@gmail.com>
2016-10-16 10:04:44 +0300

    Upd 16-11-2016

--- ProgrammingFundamentals/Objects and Classes/04.DistancebetweenPoints/DistanceBetweenPoints.cs
+++ ProgrammingFundamentals/Objects and Classes/04.DistancebetweenPoints/DistanceBetweenPoints.cs
@@ -7,9 +7,33 @@ namespace DistanceBetweenPoints
 {
     class DistanceBetweenPoints
     {
+    
         static void Main()
         {
+            Point p1 = Readpoint();
+            Point p2 = Readpoint();
+            double distance = CalcDistance(p1, p2);
 
+            Console.WriteLine($"{distance:f3}");
         }
+        static double CalcDistance(Point p1, Point p2)
+        {
+            int deltaX = p2.X - p1.X;
+            int deltaY = p2.Y - p1.Y;
+            return Math.Sqrt(Math.Pow(deltaX,2) + Math.Pow(deltaY,2));
+        }
+        static Point Readpoint()
+        {
+            int[] pointInfo = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Point point = new Point();
+            point.X = pointInfo[0];
+            point.Y = pointInfo[1];
+            return point;
+        }
+    }
+    class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
     }
 }
---
+++ ProgrammingFundamentals/Objects and Classes/06.RectanglePosition/RectanglePosition.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace RectanglePosition
+{
+    class RectanglePosition
+    {
+        static void Main()
+        {
+            Rectangle rect1 = ReadRectangle();
+            Rectangle rect2 = ReadRectangle();
+            Console.WriteLine(rect1.isInside(rect2) ? "Inside" : "Not inside");
+        }
+        static Rectangle ReadRectangle()
+        {
+            int[] rectInfo = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Rectangle rect = new Rectangle();
+            rect.Top = rectInfo[0];
+            rect.Left = rectInfo[1];
+            rect.Width = rectInfo[2];
+            rect.Height = rectInfo[3];
+            return rect;
+        }
+    }
+    class Rectangle
+    {
+        public int Top { get; set; }
+        public int Left { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public int CalcArea()
+        {
+            return Width * Height;
+        }
+        public int Bottom
+        {
+            get
+            {
+                return Top + Height;
+            }
+        }
+        public int Right
+        {
+            get
+            {
+                return Left + Width;
+            }
+        }
+        public bool isInside(Rectangle r)
+        {
+            return (r.Left <= Left) && (r.Right >= Right) && (r.Top <= Top) && (r.Bottom >= Bottom);
+        }
+    }
+}
